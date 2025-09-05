39cfec8f1e3960a4bf0f78a5b8f2bba45d51d167
23010385-commits <23010385@st.phenikaa-uni.edu.vn>
2025-09-05 17:43:29 +0700

    Update App.java

--- ourProject/src/App.java
+++ ourProject/src/App.java
@@ -2,10 +2,21 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        
 
-        testLesson.test();
+        int n = 10;
+        long startTime = System.nanoTime();
+        int result = factorial(n);
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
 
-      
+        System.out.println("factorial(" + n + ") = " + result);
+        System.out.println("User time (nanoseconds): " + duration);
+
+
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
     }
 }
