d22304f5f3ecd4813110c0c50b70593f72f9c9de
23010385-commits <23010385@st.phenikaa-uni.edu.vn>
2025-09-12 17:48:25 +0700

    update

--- ourProject/src/models/GiaoVien.java
+++ ourProject/src/models/GiaoVien.java
@@ -1,5 +1,37 @@
-
-
 public class GiaoVien {
-    
+    private int id;
+    private String ten;
+    private int tuoi;
+    private String chuyenMon;
+
+    public GiaoVien() {}
+
+    public GiaoVien(int id, String ten, int tuoi, String chuyenMon) {
+        this.id = id;
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.chuyenMon = chuyenMon;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTen() { return ten; }
+    public void setTen(String ten) { this.ten = ten; }
+
+    public int getTuoi() { return tuoi; }
+    public void setTuoi(int tuoi) { this.tuoi = tuoi; }
+
+    public String getChuyenMon() { return chuyenMon; }
+    public void setChuyenMon(String chuyenMon) { this.chuyenMon = chuyenMon; }
+
+    @Override
+    public String toString() {
+        return "Giaovien{" +
+                "id=" + id +
+                ", ten='" + ten + '\'' +
+                ", tuoi=" + tuoi +
+                ", chuyenMon='" + chuyenMon + '\'' +
+                '}';
+    }
 }
--- ourProject/src/models/Teaching.java
+++ ourProject/src/models/Teaching.java
@@ -1,5 +1,43 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Teaching {
-    
+    private GiaoVien giaovien;
+    private KhoaHoc khoahoc;
+    private List<Baihoc> baihocs;
+
+    public Teaching(GiaoVien giaovien, KhoaHoc khoahoc) {
+        this.giaovien = giaovien;
+        this.khoahoc = khoahoc;
+        this.baihocs = new ArrayList<>();
+    }
+
+    // CRUD cho bài học trong Teaching
+    public void addBaihoc(Baihoc baihoc) {
+        baihocs.add(baihoc);
+    }
+
+    public void removeBaihoc(int id) {
+        baihocs.removeIf(b -> b.getId() == id);
+    }
+
+    public void updateBaihoc(int id, String newTen) {
+        for (Baihoc b : baihocs) {
+            if (b.getId() == id) {
+                b.setTen(newTen);
+                break;
+            }
+        }
+    }
+
+    public List<Baihoc> getBaihocs() {
+        return baihocs;
+    }
+
+    @Override
+    public String toString() {
+        return "Teaching{giaovien=" + giaovien +
+                ", khoahoc=" + khoahoc +
+                ", baihocs=" + baihocs + "}";
+    }
 }
