1ad43689ca98f30b1e32411ab81e0468841d5bcd
Thu Hường <150923133+ttt-huong@users.noreply.github.com>
2025-09-12 15:56:30 +0700

    Update Lesson.java

--- ourProject/src/models/Lesson.java
+++ ourProject/src/models/Lesson.java
@@ -2,25 +2,58 @@
 
 public class Lesson {
 
-    private String lessonID;
-    private String lessonName;
+    // thuộc tính - biến thành viên
+    private int lessonID;
+    private String title;
+    private String noiDung;
+    private int thoiLuong;
 
-    Lesson(String id, String name){
-        lessonID = id;
-        lessonName = name;
+    // hàm tạo - để khởi tạo Lesson khi tạo đối tượng mới
+    public Lesson(int id, String title, String nd, int thoiLuong){
+        this.lessonID = id;
+        this.title = title;
+        this.noiDung = nd;
+        this.thoiLuong = thoiLuong;
     }
 
-    public void setLessonName(String name){
-        lessonName = name;
-    }
-
-    public String getLessonname(){
-        return lessonName;
-    }
-
-    public String getID(){
+    public int getLessonID(){
         return lessonID;
-    } 
+    }
 
-    
+    public void setLessonID( int lessonID){
+        this.lessonID = lessonID;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle( String title){
+        this.title = title;
+    }
+
+    public String getNoiDung(){
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung){
+        this.noiDung = noiDung;
+    }
+
+    public int getThoiLuong(){
+        return thoiLuong;
+    }
+
+    public void setThoiLuong( int thoiLuong){
+        this.thoiLuong = thoiLuong;
+    }
+
+    public void showInfo(){
+        System.out.println("ID: " + lessonID);
+        System.out.println("Tieu de: " + title);
+        System.out.println("Noi dung: " + noiDung);
+        System.out.println("Thoi luong: " + thoiLuong + " phut");
+        System.out.println();
+    }
+
 }
