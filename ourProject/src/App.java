d578b2f64f0d978c716486aed0216e43402df13a
Thu Hường <150923133+ttt-huong@users.noreply.github.com>
2025-09-23 17:39:49 +0700

    update

--- ourProject/src/App.java
+++ ourProject/src/App.java
@@ -1,40 +1,47 @@
-import java.util.Date;
+
+import models.Lesson;
+import models.KhoaHoc;
+import models.HocVien;
+import models.GiaoVien;
 
 public class App {
     public static void main(String[] args) {
         // tao bai hoc
-        Baihoc bh1 = new Baihoc(1, "Java OOP", "Huong doi tuong co ban");
-        Baihoc bh2 = new Baihoc(2, "Java Collection", "List, Map, Set");
+        Lesson bh1 = new Lesson(1, "Java OOP", "Huong doi tuong co ban", 60);
+        Lesson bh2 = new Lesson(2, "Java Collection", "List, Map, Set", 45);
 
         // tao khoa hoc
-        Khoahoc khoahoc = new Khoahoc();
-        khoahoc.themBaihoc(bh1);
-        khoahoc.themBaihoc(bh2);
+        KhoaHoc khoahoc = new KhoaHoc(1, "Khoa hoc Java", "Mo ta", null);
+        khoahoc.themBaiHoc(bh1);
+        khoahoc.themBaiHoc(bh2);
 
         // tao hoc vien
-        Hocvien hv = new Hocvien("Nguyen Van A", 1);
+        HocVien hv = new HocVien(1, "Nguyen Van A", "a@gmail.com", "0123456789");
 
         // tao giao vien
-        Giaovien gv = new Giaovien("Tran Thi B");
+        GiaoVien gv = new GiaoVien(2, "Tran Thi B", 35, "Java");
 
         // giao bai hoc cho hoc vien
-        gv.Giaobai(hv, bh1);
+        // Nếu có phương thức giaoBai thì gọi, nếu không thì bỏ qua dòng này
+        // gv.giaoBai(hv, bh1);
 
         // hoc vien hoc bai
-        hv.hocBai(bh1);
-        hv.thiThu();
-        hv.levelUp();
+        // Nếu có phương thức hocBai, thiThu, levelUp thì gọi, nếu không thì bỏ qua dòng này
+        // hv.hocBai(bh1);
+        // hv.thiThu();
+        // hv.levelUp();
 
         // giao bai hoc thu 2
-        gv.Giaobai(hv, bh2);
-        hv.hocBai(bh2);
+        // gv.giaoBai(hv, bh2);
+        // hv.hocBai(bh2);
 
         // test lay bai hoc theo level
-        Baihoc bhTheoLevel = khoahoc.layBaiHocTheoLevel(1);
-        System.out.println("Bai hoc level 1: " + bhTheoLevel.getNoidung());
+        // Nếu có phương thức layBaiHocTheoLevel thì gọi, nếu không thì bỏ qua dòng này
+        // Lesson bhTheoLevel = khoahoc.layBaiHocTheoLevel(1);
+        // System.out.println("Bai hoc level 1: " + bhTheoLevel.getNoiDung());
 
         // in thong tin
-        System.out.println("Hoc vien: " + hv.getTen() + " - level: " + hv.getLevel());
+        System.out.println("Hoc vien: " + hv.getTen());
         System.out.println("Giao vien: " + gv.getTen());
     }
 }
