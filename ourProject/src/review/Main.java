9bb38daae5247442c6e4482bce77b295fe8d2f71
Thu Hường <150923133+ttt-huong@users.noreply.github.com>
2025-09-26 17:56:00 +0700

    Bài tập thực hành số 6

---
+++ ourProject/src/review/Main.java
@@ -0,0 +1,8 @@
+package review;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Chạy kiểm thử lớp Sequence:");
+        // TestSequence.test(); // Uncomment this line if TestSequence is available
+    }
+}
