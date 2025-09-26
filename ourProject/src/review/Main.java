1c7d88e73b4ded03ff1e5627ba0d01b6f5adf8e7
Thu Hường <150923133+ttt-huong@users.noreply.github.com>
2025-09-26 18:02:21 +0700

    sửa file main trong bt6

--- ourProject/src/review/Main.java
+++ ourProject/src/review/Main.java
@@ -2,7 +2,6 @@ package review;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chạy kiểm thử lớp Sequence:");
-        // TestSequence.test(); // Uncomment this line if TestSequence is available
+        System.out.println("Chay kiem thu lop Sequence");
     }
 }
