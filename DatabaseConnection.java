de05255d5d26c99068d9fb27a7a03172483e13d4
23010385-commits <23010385@st.phenikaa-uni.edu.vn>
2025-10-17 17:18:24 +0700

    Update DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/myPatient", "avnadmin", "AVNS_uKZWK4Sllz5EOGYa3tD");
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_uKZWK4Sllz5EOGYa3tD";
+    
+    private static Connection connection = null;
+    
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
