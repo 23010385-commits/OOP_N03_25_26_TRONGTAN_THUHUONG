8e700c75040325ce87711b16d6c19b2504adc4b6
Thu Hường <150923133+ttt-huong@users.noreply.github.com>
2025-10-23 14:07:54 +0700

    Sửa lỗi không tìm thấy class GuitarManagementApplication khi chạy Maven và xóa tệp gs-serving-web-content

--- ourProject/src/main/java/com/guitar/management/controller/HomeController.java
+++ ourProject/src/main/java/com/guitar/management/controller/HomeController.java
@@ -1,4 +1,3 @@
-
 package com.guitar.management.controller;
 
 import org.springframework.stereotype.Controller;
@@ -6,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    
     @GetMapping("/")
-    public String home() {
+    public String index() {
         return "index";
     }
 }
